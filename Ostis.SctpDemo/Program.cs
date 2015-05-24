19f21a0ff823e4f8668fc0e0136d21399687d540
Andrey <courage.andrey@gmail.com>
2015-05-24 11:27:52 +0300

    Выполнен Code cleanup файлов демо-проекта.

--- Ostis.SctpDemo/Program.cs
+++ Ostis.SctpDemo/Program.cs
@@ -1,42 +1,15 @@
 using System;
-using Ostis.Sctp;
-using Ostis.Sctp.Commands;
-using Ostis.Sctp.Responses;
-using Ostis.Sctp.Arguments;
-using System.Diagnostics;
-using System.Collections.Generic;
-
 
 namespace Ostis.SctpDemo
 {
-	class MainClass
+    class Program
 	{
 		public static void Main (string[] args)
 		{
-		
-//			List<ScAddress> adresses = new List<ScAddress> ();
-//			Console.WriteLine ("Start to create 10000 nodes");
-//			CommandPool pool = new CommandPool ("127.0.0.1", 55770, ClientType.SyncClient);
-//			Stopwatch watch = new  Stopwatch ();
-//			watch.Start ();
-//
-//			for (int i=0; i<10000; i++) {
-//				ACommand cmd_create_node = Command.CreateNode (ElementType.sc_type_node_const);
-//				pool.Send (cmd_create_node);
-//				adresses.Add ((cmd_create_node.Response as RspCreateNode).CreatedNodeAddress);
-//			}
-//
-//			foreach (ScAddress adr in adresses ){
-//				ACommand cmd_delnode = Command.DeleteElement (adr);
-//				pool.Send (cmd_delnode);
-//			}
-//
-//
-//			watch.Stop ();
-//			Console.WriteLine ("Times elapsed: {0}", watch.Elapsed.ToString());
-			Demo demo_test = new Demo ();
-			demo_test.demoCreateNode ();
-
+            // Подробности работы приведённого примера смотри в классе Demo.
+            var demo = new Demo();
+			demo.CreateNode();
+		    Console.ReadKey();
 		}
 	}
 }
