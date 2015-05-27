340a24b2a9b2a7e59168a5495955e4664843de83
CourageAndrey <courage.andrey@gmail.com>
2015-05-27 17:51:31 +0300

    Выполнен cleanup класса CommandPool.

--- Ostis.Sctp/CommandPool.cs
+++ Ostis.Sctp/CommandPool.cs
@@ -1,89 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
-using Ostis.Sctp.AsyncClient;
-
 
 namespace Ostis.Sctp
 {
     /// <summary>
     /// Пулл команд для сервера. Соединение с сервером происходит при создании экземпляра класса
     /// </summary>
-   public class CommandPool:IDisposable
+#warning Реализовать интерфейс IDisposable как рекомендовано в MSDN
+#warning Привести к единообразию используемые типы данных - UInt32 к uint
+    public class CommandPool : IDisposable
     {
-       private List<ACommand> _commands;
-       private uint _counter;
-       private IClient _client;
-       private ResponseFactory _rspfactory;
+        private readonly List<ACommand> commands;
+        private uint counter;
+        private readonly IClient client;
+        private readonly ResponseFactory responseFactory;
 
-       /// <summary>
-       /// Возвращает значение, указывающее подключен ли клиент к серверу
-       /// </summary>
-       /// <value>
-       ///   <c>true</c> если подключен; в противном случае <c>false</c>.
-       /// </value>
-       public bool Connected
-       { 
-           get 
-           { 
-               return _client.Connected; 
-           } 
-       }
+        /// <summary>
+        /// Возвращает значение, указывающее подключен ли клиент к серверу
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> если подключен; в противном случае <c>false</c>.
+        /// </value>
+        public bool Connected
+        { get { return client.Connected; } }
 
-       /// <summary>
-       /// Инициализирует пулл команд для сервера.
-       /// </summary>
-       /// <param name="address">Адрес сервера</param>
-       /// <param name="port">Порт сервера</param>
-       /// <param name="clienttype">Тип используемого клиента (синхронный/асинхронный)</param>
-       public CommandPool(string address, int port,ClientType clienttype)
-       {
-           _client = ClientFactory.CreateClient(clienttype);
-           _client.Received += new CallBacks.ReceiveEventHandler(_client_Received);
-           _client.Connect(address, port);
-           _commands = new List<ACommand>();
-           _rspfactory = new ResponseFactory();
-           _counter = 1;
-       }
+        /// <summary>
+        /// Инициализирует пулл команд для сервера.
+        /// </summary>
+        /// <param name="address">Адрес сервера</param>
+        /// <param name="port">Порт сервера</param>
+        /// <param name="clientType">Тип используемого клиента (синхронный/асинхронный)</param>
+        public CommandPool(string address, int port, ClientType clientType)
+        {
+            client = ClientFactory.CreateClient(clientType);
+            client.Received += client_Received;
+            client.Connect(address, port);
+            commands = new List<ACommand>();
+            responseFactory = new ResponseFactory();
+            counter = 1;
+        }
 
-       void _client_Received(IClient sender, CallBacks.ReceiveEventArgs arg)
-       {
-           if (arg.ReceivedBytes.Length >= 10)
-           {
-               AResponse _resp = _rspfactory.GetResponse(arg.ReceivedBytes);
-               _commands.Find(cmd => cmd.Id == _resp.Header.Id).Response = _resp;
-              ACommand cmdforremuve = _commands.Find(cmd => cmd.Id == _resp.Header.Id);
-              _commands.Remove(cmdforremuve);
-           }
-           else
-           {
-               AResponse _resp = _rspfactory.GetResponse(arg.ReceivedBytes);
-           }
-           if (_commands.Count == 0)
-           {
-               _counter = 1;
-           }
-       }
+        private void client_Received(IClient sender, CallBacks.ReceiveEventArgs arg)
+        {
+            if (arg.ReceivedBytes.Length >= 10)
+            {
+                AResponse _resp = responseFactory.GetResponse(arg.ReceivedBytes);
+                commands.Find(cmd => cmd.Id == _resp.Header.Id).Response = _resp;
+                ACommand cmdforremuve = commands.Find(cmd => cmd.Id == _resp.Header.Id);
+                commands.Remove(cmdforremuve);
+            }
+            else
+            {
+                AResponse _resp = responseFactory.GetResponse(arg.ReceivedBytes);
+            }
+            if (commands.Count == 0)
+            {
+                counter = 1;
+            }
+        }
 
-       /// <summary>
-       /// Добавляет команду в поток команд и отправляет ее на сервер
-       /// </summary>
-       /// <param name="command">Команда</param>
-       public void Send(ACommand command)
-       {
-           command.Id = _counter;
-           _commands.Add(command);
-           _client.Send(command.BytesStream);
-           _counter++;
+        /// <summary>
+        /// Добавляет команду в поток команд и отправляет ее на сервер
+        /// </summary>
+        /// <param name="command">Команда</param>
+        public void Send(ACommand command)
+        {
+            command.Id = counter;
+            commands.Add(command);
+            client.Send(command.BytesStream);
+            counter++;
+        }
 
-       }
-
-
-       /// <summary>
-       /// Выполняет определяемые приложением задачи, связанные с удалением, высвобождением или сбросом неуправляемых ресурсов. При этом пулл отсоединяется от сервера.
-       /// </summary>
-       public void Dispose()
-       {
-           _client.Disconnect();
-       }
+        /// <summary>
+        /// Выполняет определяемые приложением задачи, связанные с удалением, высвобождением или сбросом неуправляемых ресурсов. При этом пулл отсоединяется от сервера.
+        /// </summary>
+        public void Dispose()
+        {
+            client.Disconnect();
+        }
     }
 }
