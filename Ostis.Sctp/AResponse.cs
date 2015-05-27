e2683a0502b471fbad4cd453afbf53af7b2f1436
CourageAndrey <courage.andrey@gmail.com>
2015-05-27 20:26:42 +0300

    Выполнен cleanup класса AResponse.

--- Ostis.Sctp/AResponse.cs
+++ Ostis.Sctp/AResponse.cs
@@ -1,48 +1,41 @@
-﻿
-
-namespace Ostis.Sctp
+﻿namespace Ostis.Sctp
 {
     /// <summary>
     /// Абстрактный класс ответа сервера
     /// </summary>
     public abstract class AResponse
     {
-        private byte[] _bytes;
-        private ResponseHeader _header;
+        private readonly byte[] bytes;
+        private readonly ResponseHeader header;
 
         /// <summary>
         /// Конструктор класса <see cref="AResponse"/>
         /// </summary>
-        /// <param name="bytesstream">Массив байт</param>
-        public AResponse(byte[] bytesstream)
+        /// <param name="bytes">Массив байт</param>
+        protected AResponse(byte[] bytes)
         {
-            _bytes = bytesstream;
-            byte[] _headerbytes = new byte[10];
-            if (_bytes.Length >= 10)
+            this.bytes = bytes;
+            var headerBytes = new byte[10];
+            if (bytes.Length >= 10)
             {
                 for (int index = 0; index < 10; index++)
                 {
-                    _headerbytes[index] = _bytes[index];
+                    headerBytes[index] = bytes[index];
                 }
             }
-            _header = new ResponseHeader(_headerbytes);
+            header = new ResponseHeader(headerBytes);
         }
 
         /// <summary>
         /// Возвращает массив байт
         /// </summary>
         public byte[] BytesStream
-        {
-            get { return _bytes; }
-        }
-
+        { get { return bytes; } }
 
         /// <summary>
         /// Возвращает заголовок ответа
         /// </summary>
         public ResponseHeader Header
-        {
-            get { return _header; }
-        }
+        { get { return header; } }
     }
 }
