a2d98c51a3edf1dc869e64666dd7a378bf67d957
CourageAndrey <courage.andrey@gmail.com>
2015-05-27 17:45:28 +0300

    Выполнен cleanup интерфейса IArgument.

--- Ostis.Sctp/IArgument.cs
+++ Ostis.Sctp/IArgument.cs
@@ -1,23 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Ostis.Sctp
 {
     /// <summary>
     /// Интерфейс аргумента для команд
     /// </summary>
-  public interface IArgument
+    public interface IArgument
     {
         /// <summary>
         /// Возвращает длину массива байт аргумента
         /// </summary>
-     UInt32 Length { get; }
-     /// <summary>
-     /// Возвращает массив байт аргумента
-     /// </summary>
-     /// <value>
-     byte[] BytesStream { get; }
+        UInt32 Length
+        { get; }
+
+        /// <summary>
+        /// Возвращает массив байт аргумента
+        /// </summary>
+        /// <value>
+        byte[] BytesStream
+        { get; }
     }
 }
