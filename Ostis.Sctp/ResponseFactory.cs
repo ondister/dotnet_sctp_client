c8b8aee9af5c3a61835e62c8eda3858d49a1f50c
CourageAndrey <courage.andrey@gmail.com>
2015-05-27 17:34:20 +0300

    Выполнен cleanup класса ResponseFactory.

--- Ostis.Sctp/ResponseFactory.cs
+++ Ostis.Sctp/ResponseFactory.cs
@@ -2,84 +2,84 @@
 
 namespace Ostis.Sctp
 {
-	internal class ResponseFactory
-	{
-		public AResponse GetResponse (byte[] bytesstream)
-		{
-			byte code = 0;
-			if (bytesstream.Length != 0) {
-				code = bytesstream [0];
-			}
-            
-			AResponse response = new RspUnknown (bytesstream);
-			switch (code) {
-			case 0x00:
-				response = new RspUnknown (bytesstream);
-				break;
-			case 0x01:
-				response = new RspCheckElement (bytesstream);
-				break;
-			case 0x02:
-				response = new RspGetElementType (bytesstream);
-				break;
-			case 0x03:
-				response = new RspDeleteElement (bytesstream);
-				break;
-			case 0x04:
-				response = new RspCreateNode (bytesstream);
-				break;
-			case 0x05:
-				response = new RspCreateLink (bytesstream);
-				break;
-			case 0x06:
-				response = new RspCreateArc (bytesstream);
-				break;
-			case 0x07:
-				response = new RspGetArc (bytesstream);
-				break;
-			case 0x08:
-                    //TODO:resorved
-				break;
-			case 0x09:
-				response = new RspGetLInkContent (bytesstream);
-				break;
-			case 0x0A:
-				response = new RspFindLinks (bytesstream);
-				break;
-			case 0x0B:
-				response = new RspSetLinkContent (bytesstream);
-				break;
-			case 0x0C:
-				response = new RspIterateElements (bytesstream);
-				break;
-			case 0x0E:
-				response = new RspCreateEventSubscription (bytesstream);
-				break;
-			case 0x0F:
-				response = new RspDeleteEventSubscription (bytesstream);
-				break;
-			case 0x10:
-				response = new RspEventsEmit (bytesstream);
-				break;
-			case 0xA0:
-				response = new RspFindElementById (bytesstream);
-				break;
-			case 0xA1:
-				response = new RspSetSysId (bytesstream);
-				break;
-			case 0xA2:
-				response = new RspGetStatistics (bytesstream);
-				break;
-			case 0xFE:
-				response = new RspUnknown (bytesstream);
-				break;
-			default:
-				response = new RspUnknown (bytesstream);
-				break;
+    internal class ResponseFactory
+    {
+        public AResponse GetResponse(byte[] bytes)
+        {
+            byte code = 0;
+            if (bytes.Length != 0)
+            {
+                code = bytes[0];
+            }
 
-
-			}
-			return response;
-		}
-	}
+            AResponse response = new RspUnknown(bytes);
+            switch (code)
+            {
+                case 0x00:
+                    response = new RspUnknown(bytes);
+                    break;
+                case 0x01:
+                    response = new RspCheckElement(bytes);
+                    break;
+                case 0x02:
+                    response = new RspGetElementType(bytes);
+                    break;
+                case 0x03:
+                    response = new RspDeleteElement(bytes);
+                    break;
+                case 0x04:
+                    response = new RspCreateNode(bytes);
+                    break;
+                case 0x05:
+                    response = new RspCreateLink(bytes);
+                    break;
+                case 0x06:
+                    response = new RspCreateArc(bytes);
+                    break;
+                case 0x07:
+                    response = new RspGetArc(bytes);
+                    break;
+                case 0x08:
+                    //TODO:reserved
+                    break;
+                case 0x09:
+                    response = new RspGetLInkContent(bytes);
+                    break;
+                case 0x0A:
+                    response = new RspFindLinks(bytes);
+                    break;
+                case 0x0B:
+                    response = new RspSetLinkContent(bytes);
+                    break;
+                case 0x0C:
+                    response = new RspIterateElements(bytes);
+                    break;
+                case 0x0E:
+                    response = new RspCreateEventSubscription(bytes);
+                    break;
+                case 0x0F:
+                    response = new RspDeleteEventSubscription(bytes);
+                    break;
+                case 0x10:
+                    response = new RspEventsEmit(bytes);
+                    break;
+                case 0xA0:
+                    response = new RspFindElementById(bytes);
+                    break;
+                case 0xA1:
+                    response = new RspSetSysId(bytes);
+                    break;
+                case 0xA2:
+                    response = new RspGetStatistics(bytes);
+                    break;
+                case 0xFE:
+                    response = new RspUnknown(bytes);
+                    break;
+                default:
+                    response = new RspUnknown(bytes);
+                    break;
+            }
+            return response;
+        }
+    }
 }
